3984835624ee562c4e6391c00a4029760755f9cd
Samuel Bosch-Bird <boschbsa@mail.uc.edu>
2025-07-30 23:39:57 -0400

    Add CRUD interface and implement for Team table

---
+++ Interfaces/ICRUDDAO.cs
@@ -0,0 +1,14 @@
+using GamePetApi.Models;
+
+namespace GamePetApi.Interfaces
+{
+    public interface ICRUDDAO<T>
+    {
+        int? AddItem(T item);
+        List<T> GetAllItems();
+        List<T> GetFirstFiveItems();
+        T GetItemById(int id);
+        int? RemoveItemById(int id);
+        int? UpdateItem(T item);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,32 @@
 using GamePetApi.Data;
-using Microsoft.EntityFrameworkCore; 
+using GamePetApi.Interfaces;
+using GamePetApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register the AppDbContext with EF Core using the connection string
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContext<TeamContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
+
+builder.Services.AddScoped<ICRUDDAO<TeamMember>, TeamContextDAO>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwaggerUi();
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
