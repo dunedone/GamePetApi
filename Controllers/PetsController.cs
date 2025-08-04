95e95fd016c592c248a967ea4376671fc1b34a17
Jax <vanovejn@mail.uc.edu>
2025-08-04 19:59:52 -0400

    Added PetsController with CRUD operations

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using GamePetApi.Models;
+using GamePetApi.Interfaces;
+using System.Runtime.CompilerServices;
+
+
+namespace GamePetApi.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class PetsController : ControllerBase
+    {
+        private readonly ILogger<PetsController> _logger;
+        private readonly ICRUDDAO<Pet> _context;
+
+        public PetsController(ILogger<PetsController> logger, ICRUDDAO<Pet> context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // Create
+        [HttpPost("addpet")]
+        public IActionResult Post(Pet pet)
+        {
+            var result = _context.AddItem(pet);
+            if (result > 0) return StatusCode(500, "An error occurred: There is/are " + result + " existing pet(s) with those parameters.");
+            if (result < 0) return StatusCode(500, "An error occurred while attempting to add " + pet.Name);
+            return Ok(pet.Name + " (ID " + pet.Id + ") added to database.");
+        }
+
+        // Read
+        [HttpGet("getallpets")]
+        public IActionResult Get()
+        {
+            return Ok(_context.GetAllItems());
+        }
+        // Read
+        [HttpGet("getpetbyid")]
+        public IActionResult GetById(int id)
+        {
+            if (id > 0)
+            {
+                var pet = _context.GetItemById(id);
+                if (pet is null) return NotFound("Pet of ID " + id + " does not exist.");
+                return Ok(pet);
+            }
+            return Ok(_context.GetFirstFiveItems());
+        }
+
+        // Update
+        [HttpPut("updatepet")]
+        public IActionResult Put(Pet pet)
+        {
+            var result = _context.UpdateItem(pet);
+            if (result is null) return NotFound(pet.Name + " (ID " + pet.Id + ") does not exist.");
+            if (result != 0) return StatusCode(500, "An error occurred while attempting to update " + pet.Name);
+            return Ok(pet.Name + " (ID " + pet.Id + ") updated.");
+        }
+
+        // Delete
+        [HttpDelete("deletepetbyid")]
+        public IActionResult Delete(int id)
+        {
+            var result = _context.RemoveItemById(id);
+            if (result is null) return NotFound("Pet of ID " + id + " does not exist.");
+            if (result != 0) return StatusCode(500, "An error occurred while attempting to delete pet of ID " + id);
+            return Ok("Pet of ID " + id + " removed.");
+        }
+    }
+}
